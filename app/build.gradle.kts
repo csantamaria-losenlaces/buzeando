db388d8a256a26a69f5910a2acd7a97d263c7bae
Carlos <csantamaria.losenlaces@gmail.com>
2024-04-27 19:02:31 +0200

    Solucionado error de dependencias transitivas. Aplicación compila correctamente.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.library") version "8.3.0" apply false
+    id("com.android.library") version "8.3.2" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
 
     alias(libs.plugins.androidApplication)
